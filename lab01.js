d98f280704dc8d772909dd2d7300f721e55d45d6
phongdeptrais1tg <hoanganhphong2002hcm@gmail.com>
2023-09-20 19:42:28 +0700

    p

---
+++ lab01.js
@@ -0,0 +1,45 @@
+//bai1
+
+//a
+// console.log("Phong đẹp trai số 1 thế giới")
+// console.log("Phong đẹp trai ")
+//c
+// console.log("*****")
+// console.log("*****")
+// console.log("*****")
+// console.log("*****")
+// console.log("*****")
+
+//e
+// console.log("  *  ")
+// console.log(" *** ")
+// console.log("*****")
+
+//f
+// console.log(" ** ** ")
+// console.log("*  *  *")
+// console.log("*     *")
+// console.log(" *   * ")
+// console.log("  * *  ")
+// console.log("   *   ")
+
+//bai2
+
+//a
+
+// let a = 10
+// console.log(a)
+// //b
+// let b = '10'
+// console.log(b)
+// //c
+// a = 20
+// console.log(a)
+//d
+const pi = 3.14
+console.log(pi)
+
+//e
+
+
+
