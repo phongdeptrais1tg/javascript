dea7e619e73df3727fbfd9146f6b55cd997daeaa
phongdeptrais1tg <hoanganhphong2002hcm@gmail.com>
2023-09-25 19:40:53 +0700

    p

---
+++ function.js
@@ -0,0 +1,14 @@
+function pheptoan (a,b){
+    const tong  = a + b
+    const tru  = a -b
+    const nhan = a*b
+    const chia = a/b
+    
+    return ketqua
+
+}
+const kq1  = tinhtong(2,4)
+const kq2 = tinhtong(10,34)
+console.log(kq1)
+console.log(kq2)
+
--- lab01.js
+++ lab01.js
@@ -61,16 +61,97 @@ console.log(pi)
 
 //f)
 
-let dienthoai = new Object()
-dienthoai.ten = "iphone 14"
-dienthoai.hangSX = "Apple"
-dienthoai.mausac = "Den"
-dienthoai.gia = "10000usd"
+// let dienthoai = new Object()
+// dienthoai.ten = "iphone 14"
+// dienthoai.hangSX = "Apple"
+// dienthoai.mausac = "Den"
+// dienthoai.gia = "10000usd"
 
-dienthoai.gia = "750usd"
-dienthoai.ten = "14promax"
+// dienthoai.gia = "750usd"
+// dienthoai.ten = "14promax"
 
-console.log(dienthoai)
+// console.log(dienthoai)
+
+//a)
+// let a = 5
+// let b = 7
+
+// let tong = a + b
+// let tru = a - b
+// let nhan = a * b
+// let chia = a / b
+// console.log("a + b = ",tong)
+// console.log("a - b = ",tru)
+// console.log("a * b = ",nhan)
+// console.log("a / b = ",chia)
+
+//b)
+// let a = 6
+// let b = 8
+
+// let tbc = (a + b)/2
+// console.log("Trung bình cộng",tbc)
+
+//c)
+// let x = 3
+// let y = 4
+// let z = 6
+
+// let pt1 = x**2 + 2*x + 1
+// let pt2 = x**3 - 3*x*y - 5*y + 3*y**2
+// let pt3 = (x*y)**2 - (2*(x**2)*y) + 13*y
+// let pt4 = 4*x**3 + 3*x*y + y**2 -(2*x**2 - 3*y)
+// let pt5 = ((5*(x**2)) / (4*x*y) ) + y**2
+// let pt6 = x**2 - ((2*z*x*y) / y**2) + 5*x - 2*y**2 + 4*x*z**3 + z**3
+
+
+// console.log("pt1 = ",pt1)
+// console.log("pt2 = ",pt2)
+// console.log("pt3 = ",pt3)
+// console.log("pt4 = ",pt4)
+// console.log("pt5 = ",pt5)
+// console.log("pt6 = ",pt6)
+
+//d)
+// let duongkinh = 5
+// let bankinh = duongkinh/2
+// let chuvi = (bankinh * 2) * 3.14
+// let dientich = (bankinh**2)*3.14
+
+// console.log("chu vi = ",chuvi)
+// console.log("dien tích = ",dientich)
+
+//E)
+// let chieudai = 15
+// let chieurong = 10
+// let chuvihcn = (chieudai + chieurong) *2
+// let dientichhcn = chieudai * chieurong
+
+// console.log(chuvihcn)
+// console.log(dientichhcn)
+
+//f)
+// let quang_duong = 120
+// let time = 130/60
+// let vantoc = quang_duong/time
+// console.log(vantoc)
+
+//g)
+// let time = 150/60
+// let van_toc = 60
+// let quangduong = time.van_toc
+// console.log(quangduong)
+
+//h)
+
+
+
+
+
+
+
+
+
 
 
 
