bdaeaa66215d45cad0191a0a72dc0bfbafd50207
phongdeptrais1tg <hoanganhphong2002hcm@gmail.com>
2023-09-27 19:39:10 +0700

    p

---
+++ CauDieuKien.js
@@ -0,0 +1,84 @@
+// function kiemtraAmDuong(n){
+//     if (n < 0){
+//         console.log('N âm')
+//     }
+//     else if (n = 0){
+
+//         console.log("N = 0")
+//     }
+
+//     else {
+//         console.log("N dương")
+//     }
+
+// }
+
+// kiemtraAmDuong(1)
+
+// function KtraSoChanLe(a){
+//     if (a %2 == 0) {
+//         console.log("Chẵn")
+
+//     }
+//     else{
+//         console.log("Lẻ")
+//     }
+
+// }
+// KtraSoChanLe(0)
+
+// function timsolonnhat(a,b,c){
+//     if (a > b && a > c){
+//         console.log("a lớn nhất")
+//     }
+//     else if (b > a && b > c){
+//         console.log("B lớn nhất")
+//     }
+//     else{
+//         console.log("C lớn nhất")
+//     }
+// }
+// timsolonnhat(1,2,3)
+
+// function namnhuan (c){
+//     if (c%4 ==0 && c %100 != 0){
+//         console.log("Năm nhuận")
+//     }
+//     else{
+//         console.log("Nmă ko nhuận")
+//     }
+// }
+// namnhuan(2018)
+
+// function nguyen_phu (kitu){
+//     if (kitu == 'u' || kitu == 'e' || kitu == 'o' || kitu == 'a' || kitu == 'i'){
+//         console.log("Nguyên âm")
+//     }
+//     else {
+//         console.log("Phụ âm")
+//     }
+
+
+// }
+// nguyen_phu('u')
+
+//Bai2
+//a
+function ptbc (a,b,c){
+    if (a == 0 && b ==0){
+        console.log("Phương trình vô nghiệm")
+    }
+
+    if (a= 0){
+        nghiem_1 = -c/b
+        console.log("")
+
+    }
+
+
+
+}
+
+
+
+
